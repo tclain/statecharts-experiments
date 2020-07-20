04b415cd6d883347aed6f3020419d7736d4043c6
Timothée Clain <timothee.clain@gmail.com>
2020-07-20 00:17:57 -0400

    pluralizeds composeConfigs

--- packages/compose/src/index.ts
+++ packages/compose/src/index.ts
@@ -6,6 +6,6 @@ type AnyMachineConfig = MachineConfig<any, any, any>;
  * merge multiple xstate config into a single one
  * @param configs the xstate machine config to merge
  */
-export const composeConfig = (
+export const composeConfigs = (
   ...configs: AnyMachineConfig[]
 ): AnyMachineConfig => configs[0];
