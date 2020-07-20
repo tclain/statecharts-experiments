9d506e43e9da6771c028444eec79e18709e7baf5
Timothée Clain <timothee.clain@gmail.com>
2020-07-20 00:16:42 -0400

    add docs for composeConfig

--- packages/compose/src/index.ts
+++ packages/compose/src/index.ts
@@ -2,6 +2,10 @@ import { MachineConfig } from "xstate";
 
 type AnyMachineConfig = MachineConfig<any, any, any>;
 
+/**
+ * merge multiple xstate config into a single one
+ * @param configs the xstate machine config to merge
+ */
 export const composeConfig = (
   ...configs: AnyMachineConfig[]
 ): AnyMachineConfig => configs[0];
